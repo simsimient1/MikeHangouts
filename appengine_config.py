c9a61427047126262ca9fde6b88183bea0606e69
Nurmike420 <109045595+simsimient1@users.noreply.github.com>
2024-04-02 04:21:29 +0300

    Create appengine_config.py

---
+++ appengine_config.py
@@ -0,0 +1,14 @@
+"""`appengine_config` gets loaded when starting a new application instance."""
+import vendor
+# insert `lib` as a site directory so our `main` module can load
+# third-party libraries, and override built-ins with newer
+# versions.
+vendor.add('lib')
+
+appstats_CALC_RPC_COSTS = True
+appstats_SHELL_OK = True
+
+def webapp_add_wsgi_middleware(app):
+  from google.appengine.ext.appstats import recording
+  app = recording.appstats_wsgi_middleware(app)
+  return app
